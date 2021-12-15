2cd530a74baf157777e6a7329e7601b1589ca2b4
Mikita <73610514+MikitaSerou@users.noreply.github.com>
2021-12-15 13:00:37 +0300

    side menu

--- src/main/webapp/FanficPortal/src/app/app.module.ts
+++ src/main/webapp/FanficPortal/src/app/app.module.ts
@@ -6,13 +6,12 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {AppComponent} from './app.component';
 import {UserPageComponent} from './components/user/user-page/user-page.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { LoginFormComponent } from './components/forms/login-form/login-form.component';
-import { RegistrationFormComponent } from './components/forms/registration-form/registration-form.component';
+import {LoginFormComponent} from './components/forms/login-form/login-form.component';
+import {RegistrationFormComponent} from './components/forms/registration-form/registration-form.component';
 import {AngularMaterialModule} from "./angular-material.module";
 import {AppRoutingModule} from "./app-routing.module";
 import {FlexLayoutModule} from "@angular/flex-layout";
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { FooterComponent } from './components/footer/footer.component';
+import {FooterComponent} from './components/footer/footer.component';
 
 
 @NgModule({
@@ -21,7 +20,6 @@ import { FooterComponent } from './components/footer/footer.component';
     UserPageComponent,
     LoginFormComponent,
     RegistrationFormComponent,
-    NavBarComponent,
     FooterComponent,
   ],
   imports: [
